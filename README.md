9444eaca32ffeb77962dd5a79d0cb355763f3b64
hefziben <hefziben@gmail.com>
2021-12-06 00:11:00 -0500

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # unsplashChallenge
+
+## Available Scripts
+
+In the project directory, you can run:
+
+### `npm start to install packaged` &&& node index.js to run the server
+
--- controllers/unsplash-controller.js
+++ controllers/unsplash-controller.js
@@ -1,12 +1,15 @@
 const { validationResult } = require("express-validator");
 const createProxyMiddleware = require('http-proxy-middleware');
+require('dotenv').config()
 
 const axios = require('axios');
+const apiUrl = process.env.API
+const accessKey = process.env.ACCESS_KEY
 
 const allPhotos = async(req, res, next) => {
     console.log('here');
 
-    axios.get(`https://api.unsplash.com/photos/?client_id=${process.env.ACCESSKEY}`).then((result) => {
+    axios.get(`${apiUrl}/photos/?client_id=${accessKey}&per_page=9`).then((result) => {
         console.log(result.data);
         res.json({
             msessage: 'ok',
@@ -22,7 +25,7 @@ const filterPhotos = async(req, res, next) => {
     console.log(req.params);
     console.log(req.query.filter);
     // &filter=${req.query.filter} client_id=${process.env.ACCESSKEY}
-    axios.get(`https://api.unsplash.com/search/photos?&query=${req.query.filter}&client_id=${process.env.ACCESSKEY}`).then((result) => {
+    axios.get(`${apiUrl}/search/photos?&query=${req.query.filter}&client_id=${accessKey}&per_page=9`).then((result) => {
         // console.log(result.data);
         res.json({
             msessage: 'ok',
